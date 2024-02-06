b0d19a43befacdf1c62ad8aa5dc7208c4a2b1c48
max <max.ruchkin@gmail.com>
2024-02-06 19:27:52 +0200

    style: flowbite default buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,9 @@ export const Button: FunctionComponent<PropsWithChildren<ButtonProps>> = ({
     <button
       onClick={onClick}
       type="button"
-      className={classNames("px-2 py-1 border border-black")}
+      className={classNames(
+        "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
+      )}
     >
       {children}
     </button>
