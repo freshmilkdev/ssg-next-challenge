27b6d0d76f525502fff9c36e1e6dbbe21173484a
max <max.ruchkin@gmail.com>
2024-02-06 14:55:40 +0200

    feat: Button onClick prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,17 @@
-import { FunctionComponent, PropsWithChildren } from "react";
+import { FunctionComponent, PropsWithChildren, MouseEvent } from "react";
 import classNames from "classnames";
 
-type ButtonProps = {};
+type ButtonProps = {
+  onClick(event: MouseEvent<HTMLButtonElement>): void;
+};
 
 export const Button: FunctionComponent<PropsWithChildren<ButtonProps>> = ({
   children,
+  onClick,
 }) => {
   return (
     <button
+      onClick={onClick}
       type="button"
       className={classNames("px-2 py-1 border border-black")}
     >
