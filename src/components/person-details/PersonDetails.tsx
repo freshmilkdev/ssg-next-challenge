d8c28891cd12366b9b754defd2aa27ea88807a2c
max <max.ruchkin@gmail.com>
2024-02-06 18:55:15 +0200

    feat: display error message box

--- src/components/person-details/PersonDetails.tsx
+++ src/components/person-details/PersonDetails.tsx
@@ -4,6 +4,7 @@ import { Person } from "@/utils/common/person";
 import { Button } from "@/components/Button";
 import { usePersonDetails } from "@/components/person-details/usePersonDetails";
 import { PersonCard } from "@/components/person-details/PersonCard/PersonCard";
+import { PersonErrorMessage } from "@/components/person-details/PersonCard/PersonErrorMessage";
 
 export const PersonDetails: FunctionComponent = () => {
   const { data, person, isLoading, error, onPersonSelect } = usePersonDetails();
@@ -23,24 +24,7 @@ export const PersonDetails: FunctionComponent = () => {
         )}
       >
         {error ? (
-          <div
-            className="w-full mx-4 flex items-center p-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
-            role="alert"
-          >
-            <svg
-              className="flex-shrink-0 inline w-4 h-4 me-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-            </svg>
-            <span className="sr-only">Info</span>
-            <div>
-              <p className="font-medium">{error.message}</p>
-            </div>
-          </div>
+          <PersonErrorMessage error={error} />
         ) : (
           <PersonCard data={data} person={person} isLoading={isLoading} />
         )}
