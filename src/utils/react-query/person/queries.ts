4c5caba2a76fc365f0b772f20d8382b36a10af57
max <max.ruchkin@gmail.com>
2024-02-06 18:42:44 +0200

    feat: passing signal into fetch to cancel current request if another button was pressed

--- src/utils/react-query/person/queries.ts
+++ src/utils/react-query/person/queries.ts
@@ -2,9 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import { Person } from "@/utils/common/person";
 import { PersonType } from "@/types";
 
-const fetchPersonDetails = async (person: PersonType) => {
-  const res = await fetch(`api/person?person=${person}`);
-  console.log(res);
+const fetchPersonDetails = async (person: PersonType, signal: any) => {
+  const res = await fetch(`api/person?person=${person}`, { signal });
+
   if (!res.ok) {
     throw new Error(await res.text());
   }
@@ -13,7 +13,7 @@ const fetchPersonDetails = async (person: PersonType) => {
 export const useGetPersonDetails = (person: PersonType) => {
   return useQuery({
     queryKey: ["personDetails", person],
-    queryFn: () => fetchPersonDetails(person),
+    queryFn: ({ signal }) => fetchPersonDetails(person, signal),
     enabled: !!person,
     retry: 0,
   });
