c0c6546923f2d292dc53df7786ce015b1629d8b1
max <max.ruchkin@gmail.com>
2024-02-07 13:52:38 +0200

    refactored: useGetPersonDetails hook

--- src/components/person-details/PersonDetails.tsx
+++ src/components/person-details/PersonDetails.tsx
@@ -4,10 +4,16 @@ import { usePersonDetails } from "@/components/person-details/usePersonDetails";
 import { PersonCard } from "@/components/person-details/PersonCard/PersonCard";
 import { ErrorMessage } from "@/components/ErrorMessage";
 import { PersonButtons } from "@/components/person-details/PersonButtons";
+import { useGetPersonDetails } from "@/utils/react-query/person/queries";
 
 export const PersonDetails: FunctionComponent = () => {
-  const { data, selectedPerson, isLoading, error, onPersonSelect } =
-    usePersonDetails();
+  const { selectedPerson, onPersonSelect } = usePersonDetails();
+
+  const { data, error, isLoading, isFetching, isRefetching } =
+    useGetPersonDetails(selectedPerson);
+
+  const loading = isLoading || isFetching || isRefetching;
+
   return (
     <div
       className={classNames(
@@ -19,24 +25,20 @@ export const PersonDetails: FunctionComponent = () => {
           "w-full h-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700",
           {
             "flex items-center": error,
-            "animate-pulse": isLoading,
+            "animate-pulse": loading,
           },
         )}
       >
         {error ? (
           <ErrorMessage error={error} />
         ) : (
-          <PersonCard
-            data={data}
-            person={selectedPerson}
-            isLoading={isLoading}
-          />
+          <PersonCard data={data} person={selectedPerson} isLoading={loading} />
         )}
       </div>
       <PersonButtons
         selectedPerson={selectedPerson}
         onPersonSelect={onPersonSelect}
-        isLoading={isLoading}
+        isLoading={loading}
       />
     </div>
   );
--- src/components/person-details/usePersonDetails.ts
+++ src/components/person-details/usePersonDetails.ts
@@ -4,16 +4,10 @@ import { PersonType } from "@/types";
 
 export const usePersonDetails = () => {
   const [person, setPerson] = useState<PersonType>(null);
-
-  const { data, error, isLoading, isFetching } = useGetPersonDetails(person);
-
   const handlePerson = (person: PersonType) => setPerson(person);
 
   return {
-    isLoading: isLoading || isFetching,
-    data,
     selectedPerson: person,
-    error,
     onPersonSelect: handlePerson,
   };
 };
